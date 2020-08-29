75d1070611ed6d90d838b1c62feb1ef3d014b7f0
capeieboi <capeieboi@gmail.com>
2020-08-30 02:49:37 +0530

    Some nonsense with the matches

--- display.py
+++ display.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture('test.mp4')
 #feautre extractor
 def extractfeatures(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    corners = cv2.goodFeaturesToTrack(gray,1500,0.01,0.5)
+    corners = cv2.goodFeaturesToTrack(gray,100,0.01,0.5)
     corners = np.int0(corners)
     return corners
 
@@ -15,18 +15,20 @@ def extractfeatures(img):
 
 def matcher(frame,f,last):
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
+    matches = None
     orb = cv2.ORB_create(500)
     if frame is not None:
 
         kps = [cv2.KeyPoint(x=f[0][0],y=f[0][1],_size=20)]
         kps, des = orb.compute(frame,kps)
         if last is not None:
-            matches = bf.match(des,last)
-            print(matches)
+            matches = bf.knnMatch(des,last,k=2)
+            if matches is not None:
+                for m in matches:
+                    for n in m:
+                        print(n.distance)
 
-
-    return kps,des
+    return kps,des,matches
 
 
 
@@ -45,10 +47,10 @@ while(cap.isOpened()):
         for i in corners:
             x,y = i.ravel()
             if last is None:
-                kps,des = matcher(frame,i,None)
+                kps,des,matches = matcher(frame,i,None)
             else:
-                kps,des = matcher(frame,i,last[1])
-            last = kps,des
+                kps,des,matches = matcher(frame,i,last[1])
+            last = kps,des,matches
 
             cv2.circle(frame,(x,y),3,(0,255,255),-1)
         cv2.imshow('Frame',frame)
