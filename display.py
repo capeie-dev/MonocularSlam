113c040f29b34681246e6fe43e9ef47f662fd44a
capeieboi <capeieboi@gmail.com>
2020-09-03 12:13:41 +0530

    Integrated the test file with main

--- display.py
+++ display.py
@@ -18,44 +18,35 @@ def extractfeatures(img):
 
 #function for matching features with the previous frame
 
-def matcher(frame,f,last):
+def matcher(frame,f,last1,last2):
     matches = None
     good = []
+    ptlists=[]
     if frame is not None:        
         kps, des = orb.compute(frame,f)
-        if last is not None:
-            matches = bf.knnMatch(des,last,k=2)
+        if last1 is not None:
+            matches = bf.knnMatch(des,last1,k=2)
         if matches is not None:
            
             for m,n in matches:
                 if m.distance < 0.75*n.distance:
                     good.append([m])
+                  
+                    src = (int(last2[m.trainIdx].pt[0]),int(last2[m.trainIdx].pt[1]))
+                    dst = (int(kps[m.queryIdx].pt[0]),int(kps[m.queryIdx].pt[1]))
+            
+                    ptlists.append([src,dst])
+                    
     
-    return kps,des,matches,good
+    return kps,des,matches,ptlists
 
 
-
-def returnlines(good,kps,last):
-    good = good[0][0]
-    print(good)
-    srcpts = None
-    dstpts= None
-    
-    if good is not None:
-        
-        srcpts = last[good.trainIdx].pt
-        dstpts = kps[good.queryIdx].pt
-        
-    return srcpts,dstpts
 
 #This is main, but we at capeie corp belive that main functions are not cool, also frick the lidars
 last = None
 good = None
 while(cap.isOpened()):
     ret,frame = cap.read()
-    ptlistss=[]
-
-    ptlists=[]
     kpframe = []
     
     if ret == True:
@@ -69,24 +60,18 @@ while(cap.isOpened()):
             
         
         if last is None:
-            kps,des,matches,good= matcher(frame,kpframe,None)
+            kps,des,matches,ptlists= matcher(frame,kpframe,None,None)
             
         else:
-            kps,des,matches,good = matcher(frame,kpframe,last['des'])    
-
+            kps,des,matches,ptlists = matcher(frame,kpframe,last['des'],last['kps'])    
+            
             for p in kps:
-                cv2.circle(frame,(int(p.pt[0]),int(p.pt[1])),3,(100,100,0),1)   
-            for g in good:
-                src,dst = returnlines(good,kpframe,last['kps'])
-    
-                ptlistss.append([src,dst])
-                cv2.line(frame,(int(src[0]),int(src[1])),(int(dst[0]),int(dst[1])),(255,255,0),5)
-            for lit in ptlistss:
-                src = (int(lit[0][0]),int(lit[0][1]))
-                dst = (int(lit[1][0]),int(lit[1][1]))
-                cv2.circle(frame,src,3,(0,255,0),3) 
-                cv2.line(frame,src,dst,(0,0,255),2)
-        last = {'kps':kps,'des':des,'matches':matches,'frame':frame,'good':good}
+                cv2.circle(frame,(int(p.pt[0]),int(p.pt[1])),3,(100,100,0),1)
+            for p in ptlists:
+                cv2.circle(frame,p[0],3,(255,255,0),1)
+                cv2.line(frame,p[0],p[1],(0,0,255),1)   
+
+        last = {'kps':kps,'des':des,'matches':matches,'frame':frame,'good':ptlists}
 
         
         cv2.imshow('Frame',frame)
