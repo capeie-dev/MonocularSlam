baff33b180a5e3072aa68bfbdf6f9ece54f522ff
capeieboi <capeieboi@gmail.com>
2020-09-03 16:30:38 +0530

    Added a function

--- display.py
+++ display.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np 
 
 
-cap = cv2.VideoCapture('test8.mp4')
+cap = cv2.VideoCapture('test7.mp4')
 index_params = dict(algorithm = 1, trees = 5)
 search_params = dict(checks=50)
 flann = cv2.FlannBasedMatcher(index_params,search_params)
@@ -12,7 +12,7 @@ bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
 #feautre extractor
 def extractfeatures(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    corners = cv2.goodFeaturesToTrack(gray,10000,0.01,0.1)
+    corners = cv2.goodFeaturesToTrack(gray,3000,0.01,0.1)
     corners = np.int0(corners)
     return corners
 
@@ -31,16 +31,21 @@ def matcher(frame,f,last1,last2):
             for m,n in matches:
                 if m.distance < 0.75*n.distance:
                     good.append([m])
-                  
-                    src = (int(last2[m.trainIdx].pt[0]),int(last2[m.trainIdx].pt[1]))
-                    dst = (int(kps[m.queryIdx].pt[0]),int(kps[m.queryIdx].pt[1]))
-            
-                    ptlists.append([src,dst])
-                    
-    
-    return kps,des,matches,ptlists
+    return kps,des,matches,good
 
 
+def goodpointextractor(good,kp,last):
+    ptlists = []
+    for g in good:
+        g = g[0]
+        
+        try:
+            dst = (int(kp[g.queryIdx].pt[0]),int(kp[g.queryIdx].pt[1]))
+            src = (int(last[g.trainIdx].pt[0]),int(last[g.trainIdx].pt[1]))
+            ptlists.append([src,dst])
+        except:
+            pass
+    return ptlists
 
 #This is main, but we at capeie corp belive that main functions are not cool, also frick the lidars
 last = None
@@ -63,15 +68,12 @@ while(cap.isOpened()):
             kps,des,matches,ptlists= matcher(frame,kpframe,None,None)
             
         else:
-            kps,des,matches,ptlists = matcher(frame,kpframe,last['des'],last['kps'])    
-            
-            for p in kps:
-                cv2.circle(frame,(int(p.pt[0]),int(p.pt[1])),3,(100,100,0),1)
+            kps,des,matches,good = matcher(frame,kpframe,last['des'],last['kps'])    
+            ptlists = goodpointextractor(good,kps,last['kps'])
             for p in ptlists:
-                cv2.circle(frame,p[0],3,(255,255,0),1)
-                cv2.line(frame,p[0],p[1],(0,0,255),1)   
-
-        last = {'kps':kps,'des':des,'matches':matches,'frame':frame,'good':ptlists}
+                cv2.circle(frame,p[0],3,(0,0,255),1)
+                cv2.line(frame,p[0],p[1],(0,255,0),1)
+        last = {'kps':kps,'des':des,'matches':matches,'frame':frame,'good':good}
 
         
         cv2.imshow('Frame',frame)
